088fab3db2feca92eb84bb6a4f824bda41671f69
chaminjae <beingeskimo@gmail.com>
2020-09-02 18:35:02 +0900

    mod Lat,Long

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@
 
 /* global kakao */
 import React, { Component } from "react";
-import { markerdata } from "./JSON/location";
+import { markerdata } from "./JSON/csvjson";
 
 class App extends Component {
   componentDidMount() {
+    // infoWindow
+    var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
     // 지도 띄우기
     const container = document.getElementById("myMap");
     const options = {
@@ -15,64 +17,62 @@ class App extends Component {
     };
     const map = new kakao.maps.Map(container, options);
 
-    for (var i = 0; i < markerdata.length; i++) {
-      // 변환할 객체 생성
-      var geocoder = new kakao.maps.services.Geocoder(),
-        wtmX = markerdata[i].x,
-        wtmY = markerdata[i].y;
+    var marker = new kakao.maps.Marker({
+      map: map, // 마커를 표시할 지도
+      position: new kakao.maps.LatLng(37.475926, 126.965384), // 마커를 표시할 위치
+    });
 
-      // 변환
-      geocoder.transCoord(wtmX, wtmY, transCoordCB, {
-        input_coord: kakao.maps.services.Coords.WTM, // 변환을 위해 입력한 좌표계 입니다
-        output_coord: kakao.maps.services.Coords.WGS84, // 변환 결과로 받을 좌표계 입니다
-      });
-    }
-
-    // 마커 이미지
+    // 마커 이미지 주소
     var imageSrc =
       "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
-    function transCoordCB(result, status) {
-      for (var i = 0; i < result.length; i++) {
-        // 마커 이미지의 이미지 크기 입니다
-        var imageSize = new kakao.maps.Size(24, 35);
-        // 마커 이미지 생성
-        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-        if (status === kakao.maps.services.Status.OK) {
-          const marker = new kakao.maps.Marker({
-            map: map,
-            position: new kakao.maps.LatLng(result[i].y, result[i].x),
-            title: markerdata[i].bplcnms,
-            image: markerImage,
-          });
-        }
-        console.log(result[i].y, ",", result[i].x);
-      }
+    for (var i = 0; i < markerdata.length; i++) {
+      // 마커 이미지의 이미지 크기 입니다
+      var imageSize = new kakao.maps.Size(24, 35);
+
+      // 마커 이미지를 생성합니다
+      var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+      // 마커를 생성합니다
+      var marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: new kakao.maps.LatLng(markerdata[i].lat, markerdata[i].long), // 마커를 표시할 위치
+        title: markerdata[i].locName, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image: markerImage, // 마커 이미지
+      });
+
+      // 마커에 표시할 인포윈도우를 생성합니다
+      infowindow = new kakao.maps.InfoWindow({
+        content: markerdata[i].locName, // 인포윈도우에 표시할 내용
+      });
+
+      // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+      // 이벤트 리스너로는 클로저를 만들어 등록합니다
+      // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+      kakao.maps.event.addListener(
+        marker,
+        "mouseover",
+        makeOverListener(map, marker, infowindow)
+      );
+      kakao.maps.event.addListener(
+        marker,
+        "mouseout",
+        makeOutListener(infowindow)
+      );
     }
-    // 최종 백업
-    // markerdata.forEach((el) => {
-    //   // 마커 생성
-    //   const marker = new kakao.maps.Marker({
-    //     // 마커가 표시 될 지도
-    //     map: map,
-    //     // 마커가 표시 될 위치
-    //     position: new kakao.maps.LatLng(el.x, el.y),
-    //     // 마커에 호버 시 나타날 title
-    //     title: el.bplcnms,
-    //   });
-    //   marker.setMap(map);
-    // });
-    // 여기까지
+    // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+    function makeOverListener(map, marker, infowindow) {
+      return function () {
+        infowindow.open(map, marker);
+      };
+    }
 
-    // 마커 표시 위치
-    // const markerPosition = new kakao.maps.LatLng(37.475926, 126.965384);
-
-    // // 마커 생성
-    // const marker = new kakao.maps.Marker({
-    //   position: markerPosition,
-    // });
-    // 마커를 지도 위에 표시
-    // marker.setMap(map);
+    // 인포윈도우를 닫는 클로저를 만드는 함수입니다
+    function makeOutListener(infowindow) {
+      return function () {
+        infowindow.close();
+      };
+    }
   }
 
   render() {
