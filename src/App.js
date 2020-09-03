789bce028b160c8e238cf9aac4231acdc54d5f3c
chaminjae <beingeskimo@gmail.com>
2020-09-03 20:36:45 +0900

    Header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 // velog.io/@bearsjelly
 import React, { Component } from "react";
 import Mapcomp from "./MapComp/mapComp";
+import Header from "./Header/Header";
 
 class App extends Component {
   constructor(props) {
@@ -13,6 +14,9 @@ class App extends Component {
   render() {
     return (
       <>
+        <div className="Head">
+          <Header />
+        </div>
         <div className="Body">{this.state.page === "Map" && <Mapcomp />}</div>
       </>
     );
---
+++ src/Header/Header.js
@@ -0,0 +1,42 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+// Header
+
+import React, { Component } from "react";
+import "../css/header.css";
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="divHead">
+        <div className="top-Head">
+          <b> 안녕하세요! </b>
+        </div>
+        <div className="main-Head">
+          <div className="content-Head">
+            <ul className="ul-menu">
+              <li class="li-menu">
+                <a href="/">Menu 1</a>
+              </li>
+              <li class="li-menu">
+                <a href="/">Menu 2</a>
+              </li>
+              <li class="li-menu">
+                <a href="/">Menu 3</a>
+              </li>
+              <li class="li-menu">
+                <a href="/">Menu 4</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
